2e69506c70a48b9ae20c66eef516e9d6f1d5a4a6
YoonBaek <royal729@naver.com>
2022-03-17 15:49:30 +0900

    ✨ Feat: Setup recoil, react-query, theme roots

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+
 import App from "./App";
+import { lightTheme } from "./global/theme";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={lightTheme}>
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
---
+++ frontend/src/routes/Router.tsx
@@ -0,0 +1,36 @@
+import { Badge } from "antd";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "../pages/contents/about";
+import Admin from "../pages/contents/admin";
+import Artist from "../pages/contents/artist";
+import ArtistDetail from "../pages/contents/artistDetail";
+import BadgeDetail from "../pages/contents/badgeDetail";
+import Edition from "../pages/contents/edition";
+import Main from "../pages/contents/main";
+import Minting from "../pages/contents/minting";
+import Profile from "../pages/contents/profile";
+import User from "../pages/contents/user";
+import Header from "../pages/header";
+
+export const UstRouter = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
+        <Route path="artist" element={<Artist />}>
+          <Route path=":artist-id" element={<ArtistDetail />} />
+        </Route>
+        <Route path="badge" element={<Badge />}>
+          <Route path=":badge-id" element={<BadgeDetail />} />
+        </Route>
+        <Route path="/edition/:edition-id" element={<Edition />} />
+        <Route path="user" element={<User />} />
+        <Route path="/profile/:user-id" element={<Profile />} />
+        <Route path="/minting" element={<Minting />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
