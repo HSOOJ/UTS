079477850af8c17da66098cdf3e9a157f6c5a236
연진우 <2dwd10@gmail.com>
2022-03-22 19:53:33 +0900

    ✨ Feat: NFT 뱃지 목록 Component 생성

--- frontend/src/components/contents/profile/nftBadgeList/NftBadgeList.tsx
+++ frontend/src/components/contents/profile/nftBadgeList/NftBadgeList.tsx
@@ -1,9 +1,59 @@
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { profileState } from "../../../../recoil/profile";
+import Like from "./like";
+import Own from "./own";
+import Selling from "./selling";
+
 export const NftBadgeList = () => {
+  // recoil
+  const [profileStateVal, setProfileStateVal] = useRecoilState(profileState);
+
+  // click button
+  const clickOwn = () => {
+    setProfileStateVal({
+      ...profileStateVal,
+      ownList: true,
+      sellingList: false,
+      likeList: false,
+    });
+  };
+  const clickSelling = () => {
+    setProfileStateVal({
+      ...profileStateVal,
+      ownList: false,
+      sellingList: true,
+      likeList: false,
+    });
+  };
+  const clickLike = () => {
+    setProfileStateVal({
+      ...profileStateVal,
+      ownList: false,
+      sellingList: false,
+      likeList: true,
+    });
+  };
+
+  // useEffect
+  useEffect(() => {
+    setProfileStateVal({
+      ...profileStateVal,
+      ownList: true,
+      sellingList: false,
+      likeList: false,
+    });
+  }, []);
+
   return (
     <>
-      <button>보유중</button>
-      <button>판매중</button>
-      <button>좋아요</button>
+      <h1>NFT badge List</h1>
+      <button onClick={clickOwn}>보유중</button>
+      <button onClick={clickSelling}>판매중</button>
+      <button onClick={clickLike}>좋아요</button>
+      {profileStateVal.ownList ? <Own /> : null}
+      {profileStateVal.sellingList ? <Selling /> : null}
+      {profileStateVal.likeList ? <Like /> : null}
     </>
   );
 };
