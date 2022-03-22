45e61dc986a59823e4b4d4c034d5f11646cc29a1
연진우 <2dwd10@gmail.com>
2022-03-22 19:51:31 +0900

    ✨ Feat: Dropdown 추가

--- frontend/src/components/header/navbar/Navbar.tsx
+++ frontend/src/components/header/navbar/Navbar.tsx
@@ -1,25 +1,13 @@
-import { useEffect, useState } from "react";
-import { Link, NavLink, useNavigate } from "react-router-dom";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { userState } from "../../../recoil/user";
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import DropdownCompo from "./dropdown";
 
 export const Navbar = () => {
   // recoil
-  const [userStateVal, setUserStateVal] = useRecoilState(userState);
 
   // state
   const [isArtist, setIsArtist] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
-
-  // useEffect
-  useEffect(() => {
-    let token = localStorage.getItem("token");
-    if (token === null) {
-      setUserStateVal({ ...userStateVal, login: false });
-    } else {
-      setUserStateVal({ ...userStateVal, login: true });
-    }
-  }, []);
 
   return (
     <>
@@ -29,11 +17,7 @@ export const Navbar = () => {
       <NavLink to={"/about"}>About</NavLink>
       {isArtist ? <NavLink to={"/minting"}>Mint</NavLink> : null}
       {isAdmin ? <NavLink to={"/admin"}>Admin</NavLink> : null}
-      {userStateVal.login ? (
-        <NavLink to={"/profile"}>Profile</NavLink>
-      ) : (
-        <NavLink to={"/user"}>Login</NavLink>
-      )}
+      <DropdownCompo />
     </>
   );
 };
