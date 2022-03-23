3f265b75589942a5fc02e878a7a540420a543363
YoonBaek <royal729@naver.com>
2022-03-23 14:41:16 +0900

    ✨ Feat: Add Global ThemeProvider Env

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,23 @@ import styled from "styled-components";
 import UstRouter from "./routes";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { GlobalStyle } from "./global/style";
+import { ThemeProvider } from "styled-components";
+
 import "antd/dist/antd.css";
+import { useRecoilValue } from "recoil";
+import { themeAtom } from "./recoil/theme";
 
 const UstContainer = styled.div``;
 
 function App() {
+  const theme = useRecoilValue(themeAtom);
+
   return (
     <UstContainer>
-      <GlobalStyle />
-      <UstRouter />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <UstRouter />
+      </ThemeProvider>
       <ReactQueryDevtools />
     </UstContainer>
   );
