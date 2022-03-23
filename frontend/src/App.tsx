e73c7d60eada3cc6456324d31f54df8eb7a47243
연진우 <2dwd10@gmail.com>
2022-03-23 21:52:55 +0900

    ✨ Feat: 로그인 유지기능(localstorage)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,27 @@ import { GlobalStyle } from "./global/style";
 import { ThemeProvider } from "styled-components";
 
 import "antd/dist/antd.css";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { themeAtom } from "./recoil/theme";
+import { useEffect } from "react";
+import { userState } from "./recoil/user";
 
 const UtsContainer = styled.div``;
 
 function App() {
+  // recoil
   const theme = useRecoilValue(themeAtom);
+  const [userStateVal, setUserStateVal] = useRecoilState(userState);
+
+  // useEffect
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+    if (token === null) {
+      setUserStateVal({ ...userStateVal, login: false });
+    } else {
+      setUserStateVal({ ...userStateVal, login: true });
+    }
+  }, []);
 
   return (
     <UtsContainer>
