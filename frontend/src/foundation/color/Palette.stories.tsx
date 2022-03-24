c68e77e038e88a91874942983a719e161ad78f85
YoonBaek <royal729@naver.com>
2022-03-24 16:25:55 +0900

    #S06P22A105-282 ✨ Feat: Change Gray Color Palette

--- frontend/src/foundation/color/Palette.stories.tsx
+++ frontend/src/foundation/color/Palette.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, Story } from "@storybook/react";
+import { Meta } from "@storybook/react";
 import styled from "styled-components";
 import LetterBox from "../../components/containers/letterBox/LetterBox";
 import Palette from "./Palette";
