1d8492126c3b501a8f8dfe6b7348345ea752b5f0
YoonBaek <royal729@naver.com>
2022-03-28 13:24:05 +0900

    #S06P22A105-361 ✨ Feat: Add yellow color to palette

--- frontend/src/foundation/color/Palette.ts
+++ frontend/src/foundation/color/Palette.ts
@@ -1,18 +1,26 @@
 import IPalette from "./Palette.types";
 
 const Palette: IPalette = {
+  // Gray
   Grigio100: "#F8FAFF",
   Grigio200: "#D5DDE9",
   Grigio300: "#8A929C",
   Grigio400: "#4C4E50",
   Grigio500: "#32363B",
 
+  // Black
   Nero100: "#222426",
   Nero300: "#131415",
 
+  // Red
   Rosso100: "#FF4D6B",
   Rosso200: "#EC003C",
 
+  // Yellow
+  Giallo200: "#FFE249",
+  Giallo300: "#FED136",
+
+  // Blue
   Blu100: "#BBD3FF",
   Blu200: "#759AFF",
   Blu300: "#0066FF",
