3eaa28c5582004364639d5e284bb8905edd2ac47
연진우 <2dwd10@gmail.com>
2022-03-30 17:42:25 +0900

    #S06P22A105-346 ✨ Feat: SellingCompo Style apply

--- frontend/src/components/contents/profile/nftBadgeList/selling/Selling.tsx
+++ frontend/src/components/contents/profile/nftBadgeList/selling/Selling.tsx
@@ -1,59 +1,32 @@
 import { Avatar, List } from "antd";
 import { useEffect, useState } from "react";
 import VirtualList from "rc-virtual-list";
-
-const fakeDataUrl =
-  "https://randomuser.me/api/?results=20&inc=name,gender,email,nat,picture&noinfo";
-const ContainerHeight = 400;
+import { profileState } from "../../../../../recoil/profile";
+import { useRecoilValue } from "recoil";
+import { SellingCompo } from "./SellingCompo";
+import { ButtonLoad } from "../NftBadgeList.style";
 
 export const Selling = () => {
-  // useState
-  const [data, setData] = useState([]);
+  // recoil
+  const { sellingData } = useRecoilValue(profileState);
 
-  // fetch
-  const appendData = () => {
-    fetch(fakeDataUrl)
-      .then((res) => res.json())
-      .then((body) => {
-        setData(data.concat(body.results));
-      });
-  };
-
-  // function
-  const onScroll = (e: any) => {
-    if (e.target.scrollHeight - e.target.scrollTop === ContainerHeight) {
-      appendData();
-    }
-  };
-
-  // useEffect
-  useEffect(() => {
-    appendData();
-  }, []);
+  // [
+  //   {
+  //       "editionImage": "사진",
+  //       "editionName": "1기",
+  //       "artistNickname": "아티스트",
+  //       "nftNum": 1,
+  //       "nftSeq": "1"
+  //   },
+  // ]
 
   return (
     <>
-      <h1>Selling Component</h1>
-      <List>
-        <VirtualList
-          data={data}
-          height={ContainerHeight}
-          itemHeight={47}
-          itemKey="email"
-          onScroll={onScroll}
-        >
-          {(item: any) => (
-            <List.Item key={item.email}>
-              <List.Item.Meta
-                avatar={<Avatar src={item.picture.large} />}
-                title={<a href="https://ant.design">{item.name.last}</a>}
-                description={item.email}
-              />
-              <div>Content</div>
-            </List.Item>
-          )}
-        </VirtualList>
-      </List>
+      <SellingCompo />
+      <SellingCompo />
+      <SellingCompo />
+      <SellingCompo />
+      <ButtonLoad>Load More...</ButtonLoad>
     </>
   );
 };
