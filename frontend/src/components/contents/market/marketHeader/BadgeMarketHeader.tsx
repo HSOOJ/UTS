167d587ac7bfd6bc96fedc17164eb5bf73372e5f
YoonBaek <royal729@naver.com>
2022-04-01 21:19:37 +0900

    #S06P22A105-364 ✨ Feat: Add Market place header

--- frontend/src/components/contents/market/marketHeader/BadgeMarketHeader.tsx
+++ frontend/src/components/contents/market/marketHeader/BadgeMarketHeader.tsx
@@ -1,19 +1,26 @@
 import { useRecoilValue } from "recoil";
 import { themeAtom } from "../../../../recoil/theme";
+import { Image } from "../../../containers/Image";
 import LetterBox from "../../../containers/letterBox/LetterBox";
-import { LayOut } from "./Market.styled";
+import { ImageLayOut, LayOut } from "./Market.styled";
 const BadgeMarketHeader = () => {
   const isDark = useRecoilValue(themeAtom).isDark;
   return (
     <LayOut isDark={isDark}>
+      <ImageLayOut
+        initial={{ y: -10 }}
+        animate={{ y: 10 }}
+        transition={{ repeat: Infinity, repeatType: "reverse", duration: 2.4 }}
+      >
+        <Image height="100%" src="assets/image/badge/badge.png" />
+      </ImageLayOut>
       <LetterBox size="h2" weight="bold" color="light">
-        당신만의 숨은
-      </LetterBox>
-      <LetterBox size="h2" weight="extraBold" color="light">
-        보석을
-      </LetterBox>
-      <LetterBox size="h2" weight="bold" color="light">
-        찾으세요
+        오직
+        <LetterBox weight="extraBold" size="h2" color="light">
+          {" "}
+          당신만의{" "}
+        </LetterBox>{" "}
+        숨은 보석들
       </LetterBox>
     </LayOut>
   );
--- frontend/src/components/contents/market/marketHeader/Market.styled.ts
+++ frontend/src/components/contents/market/marketHeader/Market.styled.ts
@@ -1,24 +1,34 @@
-import styled from "styled-components";
+import { motion } from "framer-motion";
+import styled, { css } from "styled-components";
 import Palette from "../../../../foundation/color/Palette";
 import { ThemeType } from "../../../../global/theme";
 
-export const LayOut = styled.div<ThemeType>`
-  display: flex;
-  position: relative;
-  border-radius: 60px;
-  width: 70%;
-  height: 120px;
-  align-items: center;
-  justify-content: center;
-  margin-top: 2.5rem;
-  margin-bottom: 2.5rem;
-  gap: 5px;
+const bannerGradient = css<ThemeType>`
   background: linear-gradient(
     140.33deg,
+    ${({ isDark }) => (isDark ? Palette.Blu300 : Palette.Blu200)},
     ${({ isDark }) => (isDark ? Palette.Blu200 : Palette.Blu100)},
-    ${Palette.Blu200}
+    ${Palette.Rosso100}
   );
-  span {
+`;
+
+export const LayOut = styled.div<ThemeType>`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  border-radius: 110px;
+  min-width: 500px;
+  width: 67%;
+  height: 220px;
+  align-items: center;
+  justify-content: space-evenly;
+  margin-top: 1.5rem;
+  padding: 30px;
+  margin-bottom: 1.5rem;
+  gap: 10px;
+  ${bannerGradient}
+  span,
+  image {
     z-index: 1;
   }
 
@@ -30,11 +40,13 @@ export const LayOut = styled.div<ThemeType>`
     width: 100%;
     height: 100%;
     filter: blur(25px);
-    background: linear-gradient(
-      140.33deg,
-      ${Palette.Blu100},
-      ${Palette.Blu200}
-    );
+    ${bannerGradient}
     transform: translateY(0.7rem) scale(0.9);
   }
+`;
+export const ImageLayOut = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 90%;
 `;
