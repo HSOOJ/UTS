7d3e2a6ef02094c32f67b25c2d6d61225424e5e9
blue8959 <blue8959@naver.com>
2022-04-04 22:44:01 +0900

    #S06P22A105-433 🐛 Fix: Fix edition nft heart

--- backend/src/routes/edition-router.ts
+++ backend/src/routes/edition-router.ts
@@ -1,5 +1,6 @@
 import artistService from "@services/artist-service";
 import editionService from "@services/edition-service";
+import heartService from "@services/heart-service";
 import nftService from "@services/nft-service";
 import saleService from "@services/sale-service";
 import userService from "@services/user-service";
@@ -35,7 +36,7 @@ router.get("/info", async (req, res, next) => {
   } else return res.status(404).json({ fail: "에디션 정보가 유효 하지 않음" });
 });
 
-async function nfts(editionSeq: number) {
+async function nfts(editionSeq: number, userSeq: number) {
   const editionInfo = await editionService.getEditionInfo(editionSeq);
   const nftInfo = await nftService.returnNFTOwner(editionSeq);
 
@@ -44,8 +45,13 @@ async function nfts(editionSeq: number) {
     for (let i = 0; i < nftInfo.length; i++) {
       const cur = nftInfo[i];
       const editionImage = await editionService.returnEditionImage(editionSeq);
-      console.log(editionImage);
       const saleInfo = await saleService.returnNft(cur.nft_nft_seq);
+      const heartInfo = await heartService.checkHeartNFT(
+        userSeq,
+        cur.nft_nft_seq
+      );
+      let isHeart = "n";
+      if (heartInfo !== null) isHeart = "y";
 
       res.push({
         edition_image: editionImage, // 에디션 사진
@@ -55,6 +61,7 @@ async function nfts(editionSeq: number) {
         nft_owner_seq: cur.user_user_seq, // 뱃지 주인 시퀀스
         nft_price: saleInfo.latest, // 현재 가격
         nft_is_soldout: saleInfo.isSale, // nft 판매 여부
+        heart: isHeart,
       });
     }
   }
@@ -63,7 +70,8 @@ async function nfts(editionSeq: number) {
 
 router.get("/nfts", async (req, res, next) => {
   const editionSeq = Number(req.query.editionSeq);
-  const result = await nfts(editionSeq);
+  const userSeq = Number(req.query.userSeq);
+  const result = await nfts(editionSeq, userSeq);
 
   if (result.length > 0) return res.status(200).json({ success: result });
   return res.status(404).json({ fail: "에디션 정보가 유효 하지 않음" });
