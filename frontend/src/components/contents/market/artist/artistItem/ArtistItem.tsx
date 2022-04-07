2be03a683864deb3e0df5aa8db8c524397044c41
연진우 <2dwd10@gmail.com>
2022-04-07 11:26:51 +0900

    ✨ Feat: axios get 추가

--- frontend/src/components/contents/market/artist/artistItem/ArtistItem.tsx
+++ frontend/src/components/contents/market/artist/artistItem/ArtistItem.tsx
@@ -13,28 +13,54 @@ export const ArtistItem = (api: IArtistItem) => {
   const isDark = useRecoilValue(themeAtom).isDark;
 
   return (
-    <Link to={api.id}>
+    <Link to={api.artist_artist_seq}>
       <Card isDark={isDark}>
         <LayOut>
           <Header
-            backgroundSrc={api.backgroundSrc}
-            profileSrc={api.profileSrc}
+            backgroundSrc={api.user_user_profile_image}
+            profileSrc={api.user_user_profile_image}
             isDark={isDark}
           />
-          <Profile name={api.name} followers={api.followers} />
+          <Profile
+            name={api.user_user_nickname}
+            followers={api.artist_artist_followers_total}
+          />
           <Sales
-            Txs={api.transActions}
-            Volume={api.volume}
-            Highest={api.highest}
+            Txs={api.artist_txs}
+            Volume={api.user_user_volume}
+            Highest={api.artist_max}
             isDark={isDark}
           />
-          <Token
+          {/* <Token
             newestDrops={api.newestDrops}
             bestSellers={api.bestSellers}
             isDark={isDark}
-          />
+          /> */}
         </LayOut>
       </Card>
     </Link>
+    // <Link to={api.id}>
+    //   <Card isDark={isDark}>
+    //     <LayOut>
+    //       <Header
+    //         backgroundSrc={api.backgroundSrc}
+    //         profileSrc={api.profileSrc}
+    //         isDark={isDark}
+    //       />
+    //       <Profile name={api.name} followers={api.followers} />
+    //       <Sales
+    //         Txs={api.transActions}
+    //         Volume={api.volume}
+    //         Highest={api.highest}
+    //         isDark={isDark}
+    //       />
+    //       <Token
+    //         newestDrops={api.newestDrops}
+    //         bestSellers={api.bestSellers}
+    //         isDark={isDark}
+    //       />
+    //     </LayOut>
+    //   </Card>
+    // </Link>
   );
 };
