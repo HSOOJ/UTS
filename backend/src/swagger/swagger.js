0d33fa3591475fcbfc0206b6677654c9192aa8d7
blue8959 <blue8959@naver.com>
2022-04-07 10:43:08 +0900

    📝 Chore: Update Swagger file

--- backend/src/swagger/swagger.js
+++ backend/src/swagger/swagger.js
@@ -1,8 +1,8 @@
 const swaggerAutogen = require("swagger-autogen")();
 const options = {
   info: {
-    title: "This is my API Document",
-    description: "이렇게 스웨거 자동생성이 됩니다.",
+    title: "Under the Sea",
+    description: "싸피 서울 6기 특화 프로젝트 - UTS",
   },
   host: "j6a105.p.ssafy.io:8080/api",
   servers: [{ url: "http://j6a105.p.ssafy.io:8080" }],
